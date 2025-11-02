605ac7e422ff316bead878713022f15532cb571b
Artur Karczmarczyk <artur@ideaspot.pl>
2025-11-02 22:43:32 +0100

    LAB G - data provider

--- tests/Entity/MeasurementTest.php
+++ tests/Entity/MeasurementTest.php
@@ -1,21 +1,45 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Tests\Entity;
 
 use App\Entity\Measurement;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 class MeasurementTest extends TestCase
 {
-    public function testGetFahrenheit(): void
+    public static function dataGetFahrenheit(): array
+    {
+        return [
+            ['0', '32'],
+            ['0.5', '32.9'],
+            ['7.2', '44.96'],
+            ['10.3', '50.54'],
+            ['-17.5', '0.5'],
+            ['17.5', '63.5'],
+            ['-100', '-148'],
+            ['-100.1', '-148.18'],
+            ['100', '212'],
+            ['100.1', '212.18'],
+        ];
+    }
+
+    #[DataProvider('dataGetFahrenheit')]
+    public function testGetFahrenheit(string $celsius, string $expectedFahrenheit): void
     {
         $measurement = new Measurement();
 
-        $measurement->setCelsius('0');
-        $this->assertEquals(32, $measurement->getFahrehneit());
-        $measurement->setCelsius('-100');
-        $this->assertEquals(-148, $measurement->getFahrehneit());
-        $measurement->setCelsius('100');
-        $this->assertEquals(212, $measurement->getFahrehneit());
+//        $measurement->setCelsius('0');
+//        $this->assertEquals(32, $measurement->getFahrehneit());
+//        $measurement->setCelsius('-100');
+//        $this->assertEquals(-148, $measurement->getFahrehneit());
+//        $measurement->setCelsius('100');
+//        $this->assertEquals(212, $measurement->getFahrehneit());
+
+        $measurement->setCelsius($celsius);
+        $this->assertEquals($expectedFahrenheit, $measurement->getFahrehneit(), "Expected $expectedFahrenheit Fahrenheit for $celsius Celsius, got {$measurement->getFahrehneit()}");
+
     }
 }
