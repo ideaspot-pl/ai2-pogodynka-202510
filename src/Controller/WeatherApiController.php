ff560af5534e25062bc129eba738901a97b78b74
Artur Karczmarczyk <artur@ideaspot.pl>
2025-11-02 22:07:20 +0100

    LAB G - wykorzystanie serwisu

--- src/Controller/WeatherApiController.php
+++ src/Controller/WeatherApiController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Measurement;
+use App\Service\WeatherUtil;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
@@ -11,13 +13,21 @@ final class WeatherApiController extends AbstractController
 {
     #[Route('/api/v1/weather', name: 'app_weather_api')]
     public function index(
+        WeatherUtil $util,
         #[MapQueryParameter('country')] string $country,
         #[MapQueryParameter('city')] string $city,
     ): JsonResponse
     {
+        $measurements = $util->getWeatherForCountryAndCity($country, $city);
+
         return $this->json([
             'city' => $city,
             'country' => $country,
+            'measurements' => array_map(fn(Measurement $m) => [
+                'date' => $m->getDate()->format('Y-m-d'),
+                'celsius' => $m->getCelsius(),
+            ], $measurements),
+
         ]);
     }
 }
